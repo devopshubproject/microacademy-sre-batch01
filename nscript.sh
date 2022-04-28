151a1beba5267798c01eddaaca83fc36bbb6e2b2
minhaz0707 <104357539+minhaz0707@users.noreply.github.com>
2022-04-28 11:39:27 +0530

    Create nscript.sh

---
+++ nscript.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+echo"This is sample math program"
+echo"we are going to perform addition"
+echo"enter first value for addition,eg x=a"
+read x
+echo"enter second value for addition, eg y=b"
+read y
+sum (x+y)
+echo" the result for addition is"
+echo"sum"
